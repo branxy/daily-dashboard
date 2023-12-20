da6de83b1055397228c8af4553f37297e70269d7
branxy <churkin.michael@gmail.com>
2023-12-20 16:25:55 +0300

    added conditional weather fetching once every 2 hrs

--- src/components/weather/WeatherApp.tsx
+++ src/components/weather/WeatherApp.tsx
@@ -1,148 +1,160 @@
-import { useEffect, useState } from "react";
-import { getWeatherData, startFetchingInterval } from "./weatherAPI";
+import { useState } from "react";
+import { fetchWeatherData } from "./weatherAPI";
 import Clock from "../clock/Clock";
 import Tooltip from "../Tooltip";
 
 type dataItem = {
-  app_temp: number;
-  aqi: number;
-  city_name: string;
-  clouds: number;
-  country_code: string;
-  datetime: string;
-  dewpt: number;
-  dhi: number;
-  dni: number;
-  elev_angle: number;
-  ghi: number;
-  gust: number;
-  h_angle: number;
-  lat: number;
-  lon: number;
-  ob_time: string;
-  pod: string;
-  precip: number;
-  pres: number;
-  rh: number;
-  slp: number;
-  snow: number;
-  solar_rad: number;
-  sources: string[];
-  state_code: string;
-  station: string;
-  sunrise: string;
-  sunset: string;
-  temp: number;
-  timezone: string;
-  ts: number;
-  uv: number;
-  vis: number;
-  weather: {
-    code: number;
-    icon: string;
-    description: string;
+  dateCreated: number;
+  data: {
+    app_temp: number;
+    aqi: number;
+    city_name: string;
+    clouds: number;
+    country_code: string;
+    datetime: string;
+    dewpt: number;
+    dhi: number;
+    dni: number;
+    elev_angle: number;
+    ghi: number;
+    gust: number;
+    h_angle: number;
+    lat: number;
+    lon: number;
+    ob_time: string;
+    pod: string;
+    precip: number;
+    pres: number;
+    rh: number;
+    slp: number;
+    snow: number;
+    solar_rad: number;
+    sources: string[];
+    state_code: string;
+    station: string;
+    sunrise: string;
+    sunset: string;
+    temp: number;
+    timezone: string;
+    ts: number;
+    uv: number;
+    vis: number;
+    weather: {
+      code: number;
+      icon: string;
+      description: string;
+    };
+    wind_cdir: string;
+    wind_cdir_full: string;
+    wind_dir: number;
+    wind_spd: number;
   };
-  wind_cdir: string;
-  wind_cdir_full: string;
-  wind_dir: number;
-  wind_spd: number;
 };
 
 export type dataType = {
   count: number;
   data: dataItem[];
 };
-
 export default function WeatherApp() {
-  const [details, setDetails] = useState<dataItem | null>(null);
+  const weatherFromLocalStorage = localStorage.getItem("dd-weather");
+  const savedWeather = weatherFromLocalStorage
+    ? JSON.parse(weatherFromLocalStorage)
+    : null;
+  const [weatherData, setWeatherData] = useState(savedWeather);
 
-  useEffect(() => {
-    getWeatherData()
-      .then((data) => {
-        setDetails(data.data[0]);
-      })
-      .catch((error) => console.error("Error fetching initial data:", error));
+  function msPassedFromLastWeatherFetch() {
+    const currentTime = Date.now();
+    return currentTime - weatherData!.dateCreated;
+  }
 
-    startFetchingInterval();
-  }, []);
+  async function fetchAndSave() {
+    const fetchedData = await fetchWeatherData();
+    if (fetchedData) {
+      setWeatherData(fetchedData);
+      return <WeatherInfo data={fetchedData.data[0]} />;
+    }
+  }
 
-  if (details) {
-    const wind = Math.round(details.gust);
-    const windDir = details.wind_cdir;
-    const humidity = details.rh;
-    const pressure = details.pres;
-    const clouds = details.clouds;
+  if (weatherData === null || msPassedFromLastWeatherFetch() >= 7200000) {
+    fetchAndSave();
+  }
+  return <WeatherInfo data={weatherData.data.data[0]} />;
+}
+
+function WeatherInfo({ data }: { data: dataItem["data"] }) {
+  if (data) {
+    const wind = Math.round(data.gust);
+    const windDir = data.wind_cdir;
+    const humidity = data.rh;
+    const pressure = data.pres;
+    const clouds = data.clouds;
     return (
       <>
-        {details && (
-          <>
-            <div className="heading-and-info">
-              <div className="heading-and-clock">
-                <div className="title-and-city">
-                  <h2>Weather</h2>
-                  <span className="title">in {details.city_name}</span>
-                </div>
-                <Clock darkMode="" />
-              </div>
-              <Tooltip
-                text={
-                  "Dynamic weather report based on Weatherbit data and Fetch API"
-                }
+        <div className="heading-and-info">
+          <div className="heading-and-clock">
+            <div className="title-and-city">
+              <h2>Weather</h2>
+              <span className="title">in {data.city_name}</span>
+            </div>
+            <Clock darkMode="" />
+          </div>
+          <Tooltip
+            text={
+              "Dynamic weather report based on Weatherbit data and Fetch API"
+            }
+          />
+        </div>
+        <div className="stats">
+          <div className="main-stats">
+            <div className="temp-and-icon">
+              <span className="temperature">
+                {data.temp > 0 ? "+" : ""}
+                {data.temp}℃
+              </span>
+              <img
+                className="icon"
+                src={`/img/${data.weather.icon}.png`}
+                alt=""
               />
             </div>
-            <div className="stats">
-              <div className="main-stats">
-                <div className="temp-and-icon">
-                  <span className="temperature">
-                    {details.temp > 0 ? "+" : ""}
-                    {details.temp}℃
-                  </span>
-                  <img
-                    className="icon"
-                    src={`/img/${details.weather.icon}.png`}
-                    alt=""
-                  />
-                </div>
-                <div className="text">
-                  <span>{details.weather.description}</span>
-                  <div className="feels-like">
-                    <span className="text">Feels like </span>
-                    <span className="temp">
-                      {details.app_temp > 0 ? "+" : ""}
-                      {details.app_temp}℃
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="secondary-stats">
-                <WeatherStatsElement
-                  img="wind"
-                  mainDigit={wind}
-                  measure=" m/s,"
-                  additionalInfo={windDir}
-                />
-                <WeatherStatsElement
-                  img="humidity"
-                  mainDigit={humidity}
-                  measure="%"
-                  additionalInfo={""}
-                />
-                <WeatherStatsElement
-                  img="pressure"
-                  mainDigit={pressure}
-                  measure=" mb"
-                  additionalInfo={""}
-                />
-                <WeatherStatsElement
-                  img="clouds"
-                  mainDigit={clouds}
-                  measure="%"
-                  additionalInfo={""}
-                />
+            <div className="text">
+              <span>{data.weather.description}</span>
+              <div className="feels-like">
+                <span className="text">Feels like </span>
+                <span className="temp">
+                  {data.app_temp > 0 ? "+" : ""}
+                  {data.app_temp}℃
+                </span>
               </div>
             </div>
-          </>
-        )}
+          </div>
+          <div className="secondary-stats">
+            <WeatherStatsElement
+              img="wind"
+              mainDigit={wind}
+              measure=" m/s,"
+              additionalInfo={windDir}
+            />
+            <WeatherStatsElement
+              img="humidity"
+              mainDigit={humidity}
+              measure="%"
+              additionalInfo={""}
+            />
+            <WeatherStatsElement
+              img="pressure"
+              mainDigit={pressure}
+              measure=" mb"
+              additionalInfo={""}
+            />
+            <WeatherStatsElement
+              img="clouds"
+              mainDigit={clouds}
+              measure="%"
+              additionalInfo={""}
+            />
+          </div>
+        </div>
       </>
     );
   }
@@ -168,10 +180,10 @@ export default function WeatherApp() {
 type WeatherStatsElementProps = {
   img: string;
   mainDigit:
-    | dataItem["gust"]
-    | dataItem["rh"]
-    | dataItem["pres"]
-    | dataItem["clouds"];
+    | dataItem["data"]["gust"]
+    | dataItem["data"]["rh"]
+    | dataItem["data"]["pres"]
+    | dataItem["data"]["clouds"];
   measure: string;
   additionalInfo: string;
 };
--- src/components/weather/weatherAPI.tsx
+++ src/components/weather/weatherAPI.tsx
@@ -1,34 +1,20 @@
-import { dataType } from "./WeatherApp";
-
-let weatherData: Promise<dataType>;
-
-async function fetchData() {
+export async function fetchWeatherData() {
+  // console.log("fetching data");
   const key = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY;
   const apiUrl = `https://api.weatherbit.io/v2.0/current?lat=59.9342802&lon=30.3350986&key=${key}`;
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    return data;
+    const parsedData = await response.json();
+    localStorage.setItem(
+      "dd-weather",
+      JSON.stringify({
+        dateCreated: Date.now(),
+        data: parsedData,
+      })
+    );
+    return parsedData;
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error;
   }
-}
-
-export function getWeatherData() {
-  if (!weatherData) {
-    weatherData = fetchData();
-  }
-  return weatherData;
-}
-
-export function startFetchingInterval() {
-  setInterval(async () => {
-    try {
-      const newData = await fetchData();
-      weatherData = newData;
-    } catch (error) {
-      console.error("Error fetching data from startFetchingInterval", error);
-    }
-  }, 5 * 60 * 60 * 1000);
 }
